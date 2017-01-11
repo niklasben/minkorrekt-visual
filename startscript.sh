c7d7b1d717d729383e05397dfcf3d3403c432b54
niklasben <nachtmahr2010@googlemail.com>
2017-01-11 13:54:00 +0100

    Initial Commit

---
+++ startscript.sh
@@ -0,0 +1,13 @@
+# !/bin/sh
+
+# Define Variables
+BASEDIR=./~
+CRAWLERDIR=${BASEDIR}/crawler
+DATADIR=${BASEDIR}/data
+WEBSITEDIR=${BASEDIR}/website
+
+# Save old JSON
+
+# Start crawler
+
+# Build all JSONs
